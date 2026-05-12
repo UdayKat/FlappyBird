5e25b37c75c4e0e1328248775fa2c7aa2f513785
UdayKat <130248246+UdayKat@users.noreply.github.com>
2026-05-11 23:59:25 -0400

    add java files

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class App{
+    public static void main(String[] args) throws Exception{
+        // game window
+        int boardWidth = 360;
+        int boardHeight = 640;
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+}
+
