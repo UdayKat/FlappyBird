8787df415b9d8a9ed71822873c683e2db6731bff
UdayKat <130248246+UdayKat@users.noreply.github.com>
2026-05-13 14:36:51 -0400

    delete comments

--- src/App.java
+++ src/App.java
@@ -11,8 +11,7 @@ public class App{
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //comment
-        //another comment
+
 
     }
 }
