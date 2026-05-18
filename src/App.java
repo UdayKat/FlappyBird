c32ca6e6174f9a46b2acd2310d1775032b9de03f
UdayKat <130248246+UdayKat@users.noreply.github.com>
2026-05-18 16:50:07 -0400

    finished project

--- src/App.java
+++ src/App.java
@@ -6,13 +6,16 @@ public class App{
         int boardWidth = 360;
         int boardHeight = 640;
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
+
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
 
